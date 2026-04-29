0581b42a6834b8f464b2f801b0f6ecde64518d05
José Edson Junior <edsonjunior9701@outlook.com>
2026-04-29 10:47:20 -0300

    reestruturação inicial do codigo

--- landing-page-profissional/src/app/app.ts
+++ landing-page-profissional/src/app/app.ts
@@ -1,22 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { About } from './components/about/about';
 import { Navbar } from './components/navbar/navbar';
-import { Hero } from './components/hero/hero';
-import { Footer } from './components/footer/footer';
-import { Contact } from './components/contact/contact';
-import { Projects } from './components/projects/projects';
+
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    About,
     Navbar,
-    Hero,
-    Footer,
-    Contact,
-    Projects
+
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- landing-page-profissional/src/app/pages/home/home.ts
+++ landing-page-profissional/src/app/pages/home/home.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { Hero } from '../../components/hero/hero';
-import { About } from '../../components/about/about';
-import { Projects } from '../../components/projects/projects';
-import { Contact } from '../../components/contact/contact';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    Hero,
-    About,
-    Projects,
-    Contact
-  ],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
